f308b74986141d298a16e7f13419b464f07748b8
vivekgupta <itguru4all@gmail.com>
2024-08-20 16:45:19 +0530

    downgrade gradle from 8.7 to 8.0

--- architecture-plugins/mvvm/build.gradle.kts
+++ architecture-plugins/mvvm/build.gradle.kts
@@ -10,14 +10,15 @@ kotlin{
     }
 }
 group = "io.github.vivekgupta4git"
-version = "1.0.2"
+version = "1.0.3"
 dependencies{
 
     implementation("androidx.room:room-common:2.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
-    implementation("com.android.tools.build:gradle:8.5.2")
+    //noinspection AndroidGradlePluginVersion
+    implementation("com.android.tools.build:gradle:8.0.0")
 
     //noinspection GradleDependency
     implementation("com.squareup:kotlinpoet:1.15.0"){
@@ -26,13 +27,13 @@ dependencies{
 
 }
 gradlePlugin{
-    website = "https://vivekgupta4git.github.io/"
-    vcsUrl = "https://github.com/vivekgupta4Git/GenerateArchitecture.git"
+    website.set("https://vivekgupta4git.github.io/")
+    vcsUrl.set("https://github.com/vivekgupta4Git/GenerateArchitecture.git")
     plugins.create("mvvmArch") {
         id = "io.github.vivekgupta4git.mvvm-arch"
         displayName = "Mvvm Architecture Plugin"
         implementationClass = "MvvmArchPlugin"
-        tags = listOf("mvvm","android","architecture")
+        tags.set(listOf("mvvm","android","architecture"))
         description = "Often implementing mvvm architecture in the android, there are always " +
                 "a boilerplate code. Such as Mapper for converting models from the network to the domain," +
                 "creating data sources and data repositories etc. This plugin aims to reduce the boilerplate such codebase "
--- architecture-plugins/mvvm/src/main/kotlin/architecture/AndroidExtension.kt
+++
@@ -1,5 +0,0 @@
-package architecture
-
-import com.android.build.api.dsl.CommonExtension
-
-typealias AndroidExtension = CommonExtension<*, *, *, *,*,*>
